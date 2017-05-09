733ce335aa4e76ebba3ba9274ad8d3d5680f1b82
dylward757 <dylward757@gmail.com>
2017-05-09 12:19:00 -0600

    rev 05/09/17

--- src/Runner.java
+++ src/Runner.java
@@ -4,6 +4,8 @@ public class Runner {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello! This project is from Dylan Ward");
+		System.out.println("My first rev, 5/9/17");
+
 		
 	}
 
