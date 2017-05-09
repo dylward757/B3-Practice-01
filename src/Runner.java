38d8e37b7adfafe874a50c88075d5a91b245a047
dylward757 <dylward757@gmail.com>
2017-05-09 12:31:08 -0600

    second rev 05/09/17

--- src/Runner.java
+++ src/Runner.java
@@ -5,6 +5,8 @@ public class Runner {
 		
 		System.out.println("Hello! This project is from Dylan Ward");
 		System.out.println("My first rev, 5/9/17");
+		System.out.println("My Second rev, 5/9/17");
+
 
 		
 	}
